32f179b95cd8d8c023799bc15655487e4788f51c
Luis F K Andriolo <luisfandriolo@gmail.com>
2023-08-27 17:03:46 -0300

    fix: error rendering

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 function Home() {
   return (
-    <h1>Gabriela, saiba que eu amo você! ❤😊💕</h1>
-    <h3>Você é o <strong>amor da minha vida</strong></h3>
+    <div>
+      <h1>Gabriela, saiba que eu amo você! ❤😊💕</h1>
+      <h3>
+        Você é o <strong>amor da minha vida</strong>
+      </h3>
+    </div>
   );
 }
 
