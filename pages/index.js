1fa5e82ab9b7f22a9e853a4122048397f1f8014e
Luis F K Andriolo <luisfandriolo@gmail.com>
2023-08-27 17:02:31 -0300

    first change

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-  return <h1>TESTE</h1>;
+  return (
+    <h1>Gabriela, saiba que eu amo você! ❤😊💕</h1>
+    <h3>Você é o <strong>amor da minha vida</strong></h3>
+  );
 }
 
 export default Home;
